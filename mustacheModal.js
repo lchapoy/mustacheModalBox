13cc97c84ecebceafbfb825bb010e4f2e70feca6
luis chapoy <luis_chapoy@hotmail.com>
2016-06-08 14:32:18 -0700

    Separate into Module

--- mustacheModal.js
+++ mustacheModal.js
@@ -1,18 +1,20 @@
-
-var modalButton=document.getElementById("modalButton");
-
 var launchModal=(function(){
+	var modalButton=document.getElementById("modalButton");
     var modalWindow=document.createElement('div');
     modalWindow.id="dialog-box";
     document.body.appendChild(modalWindow);
+	modalButton.addEventListener("click",function(){
+		dialogBox.style.display='block';
+	});
   return function(confObj){
         var acceptFunction=function(){
             confObj.acceptCallback();
-            alert("You click on confirm button")
+			modalWindow.style.display='none';
+           // alert("You click on confirm button")
         };
        var cancelFunction=function(){
            confObj.cancelCallback();
-            alert("You click on cancel button");
+            //alert("You click on cancel button");
             modalWindow.style.display='none';
          };
 
@@ -25,7 +27,3 @@ var launchModal=(function(){
     }
 })();
 
-var dialogBox=launchModal({title:"Dialog Box Text",message:"new Message",acceptCallback:function(){console.log("accept")},cancelCallback:function(){console.log("cancel")}});
-modalButton.addEventListener("click",function(){
-    dialogBox.style.display='block';
-});
